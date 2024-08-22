df50ecb49ffd1a1b93a8b7484398602f9a7a948d
seansky2020 <mphomuila@gmail.com>
2024-08-22 17:21:33 +0200

    commit

---
+++ knowledgebase.py
@@ -0,0 +1,4 @@
+# Simulated knowledgebase
+knowledgebase = {
+  "FAQ's":
+    "how to change trading account currency": "If you wish to change your trading account currency, you can easily do so by opening a new trading account on our client portal at https://portal.rcgmarkets.com/. After logging in, proceed to 'Account Settings' and navigate to 'Trading Accounts.' From there, you can open a new trading account and select the currency of your choice. Please be aware that you cannot modify the currency of an existing account, but opening a new account enables you to choose your preferred currency. If you have any inquiries or require assistance, please contact us!",
