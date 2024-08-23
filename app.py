3914edcb374d5af657b7de4051ad5a86ef14bf6e
seansky2020 <mphomuila@gmail.com>
2024-08-23 11:57:10 +0200

    commit

--- app.py
+++ app.py
@@ -31,20 +31,19 @@ creds = service_account.Credentials.from_service_account_file(
 sheets_service = build('sheets', 'v4', credentials=creds)
 
 # Knowledgebase setup
-knowledgebase =  {"FAQ's":
-    
-    "how to deposit"; "To deposit funds into your account, simply log in to the Client Portal using this link https://portal.rcgmarkets.com/. Once you're logged in, navigate to the 'Fund Account' section. Here, you'll see all the available deposit methods. Select your preferred method and follow the instructions to complete your deposit.",
-    
-    "account verification - struggling to upload (alternative methods to submit documents)" "If you're struggling to upload your documents for account verification, you can alternatively submit them by sending an email to fica@rcgmarkets.com or by using our WhatsApp platform at +27824016338. We'll assist you further from there!",
-    
-    "withdrawal turnaround time" "Withdrawals take 24-48 working hours.",
-    
-    "why was my pdf bank statement declined" "Your bank statement may be declined for several reasons: 1) It does not have a stamp. 2) It has different names from your client portal. 3) It does not have transactions (in this case, request a bank confirmation letter). 4) It is outdated (please send the most recent bank statement that is not older than three months). 5) It is blurry (kindly send the original PDF bank statement).",
-    
-    "available account types" "We offer the following account types: 1) Classic ; MT4 account, 2) ECN ; MT4 account, 3) Raw ; MT4 account, 4) Royal 100 ; MT5 account (bonus), 5) Royal cent ; MT5 cent account",
-    
-  "how to change trading account password" "To change your trading account password: 1) Log in to your client portal at https://portal.rcgmarkets.com/. ; 2) Navigate to the 'Account Settings' tab. 3) Click on 'Change Password'. 4) Select 'Change Trading Account Password' and choose the trading account for which you want to change the password. 5) Click 'Submit'."
- }
+knowledgebase =  {"FAQ's" 
+                  
+                  "how to deposit" "To deposit funds into your account, simply log in to the Client Portal using this link https://portal.rcgmarkets.com/. Once you're logged in, navigate to the 'Fund Account' section. Here, you'll see all the available deposit methods. Select your preferred method and follow the instructions to complete your deposit."
+                  
+                  "account verification - struggling to upload (alternative methods to submit documents)" "If you're struggling to upload your documents for account verification, you can alternatively submit them by sending an email to fica@rcgmarkets.com or by using our WhatsApp platform at +27824016338. We'll assist you further from there!"
+                  
+                  "withdrawal turnaround time" "Withdrawals take 24-48 working hours."
+                  
+                  "why was my pdf bank statement declined" "Your bank statement may be declined for several reasons 1) It does not have a stamp. 2) It has different names from your client portal. 3) It does not have transactions (in this case, request a bank confirmation letter). 4) It is outdated (please send the most recent bank statement that is not older than three months). 5) It is blurry (kindly send the original PDF bank statement)."
+                  
+                  "available account types" "We offer the following account types 1) Classic  MT4 account, 2) ECN  MT4 account, 3) Raw  MT4 account, 4) Royal 100  MT5 account (bonus), 5) Royal cent  MT5 cent account"
+                  
+                  "how to change trading account password" "To change your trading account password 1) Log in to your client portal at https://portal.rcgmarkets.com/.  2) Navigate to the 'Account Settings' tab. 3) Click on 'Change Password'. 4) Select 'Change Trading Account Password' and choose the trading account for which you want to change the password. 5) Click 'Submit'."}
 
 
 
