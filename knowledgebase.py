605fcfaf8771d3763e417dc85bbdeb46a5de4ea3
seansky2020 <mphomuila@gmail.com>
2024-08-26 09:27:31 +0200

    commit

--- knowledgebase.py
+++ knowledgebase.py
@@ -1,15 +1,15 @@
 # Simulated knowledgebase
 knowledgebase = {"FAQ's" 
                   
-                  "how to deposit" "To deposit funds into your account, simply log in to the Client Portal using this link https://portal.rcgmarkets.com/. Once you're logged in, navigate to the 'Fund Account' section. Here, you'll see all the available deposit methods. Select your preferred method and follow the instructions to complete your deposit."
+                  "Question 1 -how to deposit": "Answer 1 -To deposit funds into your account, simply log in to the Client Portal using this link https://portal.rcgmarkets.com/. Once you're logged in, navigate to the 'Fund Account' section. Here, you'll see all the available deposit methods. Select your preferred method and follow the instructions to complete your deposit.",
                   
-                  "account verification - struggling to upload (alternative methods to submit documents)" "If you're struggling to upload your documents for account verification, you can alternatively submit them by sending an email to fica@rcgmarkets.com or by using our WhatsApp platform at +27824016338. We'll assist you further from there!"
+                  "Question 2 -account verification - struggling to upload (alternative methods to submit documents)" : "Answer 2 -If you're struggling to upload your documents for account verification, you can alternatively submit them by sending an email to fica@rcgmarkets.com or by using our WhatsApp platform at +27824016338. We'll assist you further from there!",
                   
-                  "withdrawal turnaround time" "Withdrawals take 24-48 working hours."
+                  "Question 3 -withdrawal turnaround time" :"Answer 3 -Withdrawals take 24-48 working hours.",
                   
-                  "why was my pdf bank statement declined" "Your bank statement may be declined for several reasons 1) It does not have a stamp. 2) It has different names from your client portal. 3) It does not have transactions (in this case, request a bank confirmation letter). 4) It is outdated (please send the most recent bank statement that is not older than three months). 5) It is blurry (kindly send the original PDF bank statement)."
+                  "Question 4 -why was my pdf bank statement declined" : "Answer 4 -Your bank statement may be declined for several reasons 1) It does not have a stamp. 2) It has different names from your client portal. 3) It does not have transactions (in this case, request a bank confirmation letter). 4) It is outdated (please send the most recent bank statement that is not older than three months). 5) It is blurry (kindly send the original PDF bank statement).",
                   
-                  "available account types" "We offer the following account types 1) Classic  MT4 account, 2) ECN  MT4 account, 3) Raw  MT4 account, 4) Royal 100  MT5 account (bonus), 5) Royal cent  MT5 cent account"
+                  "Question 5 -available account types" : "Answer 5 -We offer the following account types 1) Classic  MT4 account, 2) ECN  MT4 account, 3) Raw  MT4 account, 4) Royal 100  MT5 account (bonus), 5) Royal cent  MT5 cent account",
                   
-                  "how to change trading account password" "To change your trading account password 1) Log in to your client portal at https://portal.rcgmarkets.com/.  2) Navigate to the Account Settings tab. 3) Click on Change Password. 4) Select Change Trading Account Password and choose the trading account for which you want to change the password. 5) Click 'Submit'."
+                  "Question 6 -how to change trading account password" : "Answer 6 -To change your trading account password 1) Log in to your client portal at https://portal.rcgmarkets.com/.  2) Navigate to the Account Settings tab. 3) Click on Change Password. 4) Select Change Trading Account Password and choose the trading account for which you want to change the password. 5) Click 'Submit'."
                   }
